c1f72cee8835334e479ecf15d7e8bdbe1b1915e4
Glyn Leine <g.m.leine@gmail.com>
2024-01-26 23:20:48 +0100

    Add photon, delete tutorial and sample scene

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        Debug.Log("Connecting");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
