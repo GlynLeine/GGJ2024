1c123cbb1461a0600b79cdd198c0e5d49575e788
Noah Brinkman <noahpbrinkman@gmail.com>
2024-01-27 20:56:49 +0100

    Start of gasthrower and health

---
+++ client/Assets/Scripts/Weapons/GasThrower.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GasThrower : Weapon_Ranged
+{
+    
+
+    public override void Attack()
+    {
+            Debug.Log("psssssssssssssssssh");
+        //Emit particles/VFX
+        
+    }
+}
