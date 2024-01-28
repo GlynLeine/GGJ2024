c369da8d2b68df0144eb1eeea4546e1399807c03
Rowan Ramsey <rowanramsey18@gmail.com>
2024-01-28 13:38:57 +0100

    main menu now goes to the gymscene, and data should be being read?

--- client/Assets/Scripts/Networking/GameData.cs
+++ client/Assets/Scripts/Networking/GameData.cs
@@ -1,42 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 struct Score
 {
     public string name;
-    public int score;
+    public float score;
 
-    public Score(string _name, int _score)
+    public Score(string _name, float _score)
     {
         name = _name;
         score = _score;
     }
 }
+
 public class GameData : MonoBehaviour
 {
+    [SerializeField] private Button m_startButton;
     [SerializeField] private string gameSceneName;
     [SerializeField] private string mainMenuSceneName;
 
-    public static Dictionary<string, int> playerScores = new Dictionary<string, int>();
+    [SerializeField] private TMP_InputField m_playerName;
+    string PlayerName;
+    public static Dictionary<string, float> playerScores = new Dictionary<string, float>();
 
-    private void Start()
+    private void Awake()
     {
+        GameData data = FindObjectOfType<GameData>();
+        if (data)
+        {
+            Destroy(data.gameObject);
+        }
         DontDestroyOnLoad(gameObject);
         SceneManager.sceneLoaded += OnSceneLoaded;
+        m_startButton.onClick.AddListener(() =>
+        {
+            PlayerName = m_playerName.text;
+        });
     }
 
-    public static void AddScore(string name, int score)
+    public static void AddScore(string name, float score)
     {
         playerScores.Add(name, score);
     }
 
-    public static (string name, int score) GetHighestScore()
+    public static (string name, float score) GetHighestScore()
     {
-        int MaxScore = 0;
+        LoadScoresFromFile();
+        float MaxScore = 0;
         string topPlayer = "";
         foreach (var pair in playerScores)
         {
@@ -81,6 +96,8 @@ public class GameData : MonoBehaviour
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
+    TimeScore timeScore;
+
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         if (scene.name.Equals(mainMenuSceneName) && playerScores.Count > 0)
@@ -89,5 +106,17 @@ public class GameData : MonoBehaviour
             FindAnyObjectByType<TopScorererUI>().topScorererName.text = $"{pair.name}: {pair.score}";
         }
 
+        if (scene.name.Equals(gameSceneName))
+        {
+            timeScore = FindObjectOfType<TimeScore>();
+            timeScore.onGameEnd.AddListener(OnGameEnd);
+        }
+
+    }
+
+    void OnGameEnd()
+    {
+        playerScores.Add(PlayerName, timeScore.currentTime);
+        WriteScoresToFile();
     }
 }
